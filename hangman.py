9dd188efcee0c628f242dfd5d4f4a289dc12acde
荒木輝 <arakiakira@arakiakiranoMacBook-puro.local>
2018-07-31 15:07:59 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,37 @@
+def hangman(word):
+    wrong = 0
+    stages = ['',
+              '___________             ',
+              '|                       ',
+              '|          |            ',
+              '|          o            ',
+              '|         /|\           ',
+              '|         / \           ',
+              '|                       ']
+
+    rletters = list(word)
+    board = ['_'] * len(word)
+    win = False
+    print('welcome to Hangman!')
+
+    while wrong < len(stages) - 1:
+        print('\n')
+        msg = '１文字を予想してね'
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(' '.join(board))
+        e = wrong + 1
+        print('\n'.join(stages[0:e]))
+        if "_" not in board:
+            print('You Win!')
+            print(' '.join(board))
+            win = True
+            break
+    if not win:
+            print('\n'.join(stages[0:wrong+1]))
+            print("You Died! The Answer is {}.".format(word))
